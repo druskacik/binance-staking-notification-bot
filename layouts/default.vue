06a0e37b26d1a6a8d310266b44836e2ae1dd70cf
RobertDruska <robert.druska@gmail.com>
2021-08-25 18:57:34 +0200

    font swap

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,13 +11,12 @@
 @font-face {
   font-family: 'nunito';
   src: url('../assets/Nunito-Regular.ttf');
+  font-display: swap;
 }
 
 #app {
   font-family: 'nunito', Avenir, Helvetica, Arial, sans-serif;
   font-size: 1rem;
-  /* -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale; */
   text-align: center;
   color: #3A3A3A;
   background-color: #F7FAFB;

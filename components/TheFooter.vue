ccfd70d7f4026eb2688dbf6d57c81e0034dbb1e9
RobertDruska <robert.druska@gmail.com>
2021-12-13 14:25:35 +0100

    community link on web and in telegram messages

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="app-footer">
+        <!-- <div>
+            Contact:
+            <a class="app-footer__link" href="https://t.me/rdruska" target="_blank">
+                https://t.me/rdruska
+            </a>
+        </div> -->
         <div>
-            Contact: https://t.me/rdruska
+            Join our community:
+            <a class="app-footer__link umami--click--community-link" href="https://t.me/bswatcher" target="_blank">
+                https://t.me/bswatcher
+            </a>
         </div>
     </div>
 </template>
@@ -11,7 +20,15 @@
 .app-footer {
     background-color: black;
     color: whitesmoke;
-    padding: 1.1rem 0;
+    padding: 1.2rem 0;
+}
+
+.app-footer__link {
+    color: whitesmoke;
+}
+
+.app-footer__link:hover {
+    color: #DBDBDB;
 }
 
 </style>

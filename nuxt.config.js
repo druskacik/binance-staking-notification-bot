ef7888b895daf962ef7f12db3adf855fb6c0833d
RobertDruska <robert.druska@gmail.com>
2022-01-06 13:59:54 +0100

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
             { name: 'format-detection', content: 'telephone=no' },
         ],
         link: [
-            // { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' },
         ],
         script: [
             {

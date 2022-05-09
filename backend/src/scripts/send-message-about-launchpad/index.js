848af2ca552bf393a4f62ee28ef55bf1267810c8
RobertDruska <robert.druska@gmail.com>
2022-05-09 23:33:58 +0200

    bulk message about launchpad

---
+++ backend/src/scripts/send-message-about-launchpad/index.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const Mustache = require('mustache');
+
+const knex = require('../../../connection');
+const readFileAsync = require('../../utils/read-file-async');
+
+const sendBulkMessageAboutLaunchpad = async () => {
+    try {
+        const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
+
+        const telegramUsers = await knex('user')
+            .whereNotNull('telegram_chat_id')
+            .select();
+
+        const templateText = await readFileAsync(__dirname + '/message.mustache');
+        const text = Mustache.render(templateText);
+
+        const messageParams = {
+            parse_mode: 'HTML',
+            text,
+            disable_web_page_preview: 1,
+        };
+
+        await Promise.all(telegramUsers.map(async (user) => {
+            try {
+                await axios.get(url, {
+                    params: {
+                        chat_id: user.telegram_chat_id,
+                        ...messageParams,
+                    },
+                });
+            } catch (err) {
+                console.log('ERROR, BOT WAS BLOCKED BY THE USER');
+            }
+        }));
+    } catch (err) {
+        console.log(err);
+    } finally {
+        process.exit();
+    }
+};
+
+sendBulkMessageAboutLaunchpad();
